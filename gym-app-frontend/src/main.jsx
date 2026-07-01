cbfd7cee2c2f2294c9dc9f6493aad36da23b4d04
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-01 15:18:46 +0300

    Update import paths for App and AuthContext

--- gym-app-frontend/src/main.jsx
+++ gym-app-frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "src/App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "src/context/AuthContext.jsx";
 
 /**
  * =========================
